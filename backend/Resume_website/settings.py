79c63da36629685c1b74879c8b3b1bcad7267981
black2025 <oghab2025@gmail.com>
2025-12-03 18:45:57 +0330

    make migrations and fixing some issue in settings about user model

--- backend/Resume_website/settings.py
+++ backend/Resume_website/settings.py
@@ -116,7 +116,7 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'account.User'
+AUTH_USER_MODEL = 'account.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
