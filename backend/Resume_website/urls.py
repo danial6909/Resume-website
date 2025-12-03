a333c8ae81883e9eaf7e76f42e187c70cbd99900
black2025 <oghab2025@gmail.com>
2025-12-03 19:10:09 +0330

    test

--- backend/Resume_website/urls.py
+++ backend/Resume_website/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import settings
+# from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('testi/', include('account.urls')),
+    # path('')
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,6 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-class RegisterView(APIView):
-    pass
+class ExampleAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        print(data)
+        return HttpResponse(data.values())
