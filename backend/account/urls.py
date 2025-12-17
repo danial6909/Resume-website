4765b385c6f44f8b0deffb650f00b3cc56bdde5f
black2025 <oghab2025@gmail.com>
2025-12-17 13:52:10 +0330

    urls remocer exampleview

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -4,7 +4,6 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from .views import RegisterAPIView
 
 urlpatterns = [
-    path('testapi/', views.ExampleAPIView.as_view()),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', RegisterAPIView.as_view(), name='register'),
