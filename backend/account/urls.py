0b2638427433e2bd81349ffae436f5f25151754e
black2025 <oghab2025@gmail.com>
2025-12-17 14:25:30 +0330

    django downgraded

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-from .views import RegisterAPIView
+
 
 urlpatterns = [
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register/', RegisterAPIView.as_view(), name='register'),
+    path('api/register/', views.RegisterAPIView.as_view(), name='register'),
     path('profile/', views.UserProfileAPIView.as_view(), name='profile'),
     path('change_credentials/', views.UserCredentialsUpdateAPIView.as_view(), name='credentials'),
     path('change_password/', views.PasswordChangeAPIView.as_view(), name='change_password'),
