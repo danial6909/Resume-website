945531300f5b00b45a91c04feeb2204f27e1205b
Ali Kelidari <oghab2025@gmail.com>
2026-02-13 19:17:11 +0330

    static file is set correct

--- backend/Resume_website/settings.py
+++ backend/Resume_website/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'StaticRoot')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # these settings related to static must change when deploying
