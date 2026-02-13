a18d334cea481b5b0a24ea55db6abacab1a056f5
Ali Kelidari <oghab2025@gmail.com>
2026-02-13 20:58:53 +0330

    Users added to Admin Panel

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'is_staff', 'is_active',]
+    fieldsets = UserAdmin.fieldsets +(
+    (None, {'fields': ('phone_number',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets +(
+        (None, {'fields': ('email', 'phone_number',)}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
