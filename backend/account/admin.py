9dfa92c9abc9ea885683f562aeae07674a8fe4d3
Ali Kelidari <oghab2025@gmail.com>
2026-02-16 03:44:19 +0330

    Users in Admin panel is optimized

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 # Register your models here.
 @admin.register(CustomUser)
-class CustomUserAdmin(ModelAdmin):
-    list_display = ['username', 'email', 'is_staff', 'is_active',]
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'phone_number', 'is_staff', 'is_active']
     search_fields = ['username', 'email', 'phone_number']
-    list_filter = ['is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active', 'is_superuser']
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('email', 'phone_number')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'phone_number', 'password'),
+        }),
+    )
